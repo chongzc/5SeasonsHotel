8c3083bde2d31256b20bd1220d2a059315f9f12d
SpicyLollipop <143978518+SpicyLollipop@users.noreply.github.com>
2024-03-10 23:08:32 +0800

    Add files via upload

---
+++ 5SeasonsHotel/5SeasonsHotel/includes/navigationMenu.php
@@ -0,0 +1,13 @@
+<div class="navigationMenu">
+
+    <img class="logo" src="\5SeasonsHotel\includes\includesImages\navigationMenuImages\logo.png" alt="logo">
+
+    <nav>
+        <ul>
+            <li><a href="/5SeasonsHotel/"> Home</a></li>
+            <li><a href="/5SeasonsHotel/Gallery/index.php"> Gallery</a></li>
+            <li><a href="/5SeasonsHotel/contactUs/index.php"> Contact Us</a></li>
+            <li><a href="/5SeasonsHotel/bookNow"> Book Now</a></li>
+        </ul>
+    </nav>
+</div>
---
+++ 5SeasonsHotel/5SeasonsHotel/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>5 Season Hotel | Home Page</title>
+    <link rel="stylesheet" type="text/css" href="\5SeasonsHotel\includes\includeStyle\navigationMenu.css">
+    <link rel="stylesheet" type="text/css" href="\5SeasonsHotel\homePage\homePageStyle.css">
+  </head>
+
+  <body>
+      <?php include('includes/navigationMenu.php');?>
+      
+      <section>
+      <div class="banner">
+        <img src="\5SeasonsHotel\homePage\homePageImages\homePageBanner.jpg">
+          <div class="homepageSlogan fade-in">
+            <h1>WELCOME TO 5 SEASONS HOTEL</h1>
+              <p> Elevate Your Stay,<br> Experience the Pinnacle of Luxury at 5 Seasons Hotel.</p>
+                <div>
+                  <a href="/5SeasonsHotel/gallery/index.php"> 
+                    <button type="button"> <span> </span> LEARN MORE </button>
+                  </a>
+
+                  <a href="/5SeasonsHotel/bookNow/index.php"> 
+                    <button type="button"> <span> </span> BOOK NOW </button>
+                  </a>
+                </div>
+          </div>
+      </div>
+
+      </section>
+      
+      <?php include('includes/footer.php');?>
+  </body>
+</html>
