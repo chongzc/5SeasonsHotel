f68b30f6f0f1f18b2edfb253de115185935d7feb
chongzc <126367554+chongzc@users.noreply.github.com>
2024-03-24 16:55:06 +0800

    Add files via upload

---
+++ 5SeasonsHotel/bookNow/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>5 Seasons Hotel | Booking Page</title>
+    <link rel="stylesheet" type="text/css" href="\5SeasonsHotel\includes\includeStyle\navigationMenu.css">
+    
+  </head>
+
+  <body>
+      <?php include('../includes/navigationMenu.php');?>
+      
+      <?php include('../includes/footer.php');?>
+  </body>
+</html>
---
+++ 5SeasonsHotel/gallery/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>5 Seasons Hotel | Gallery</title>
+    <link rel="stylesheet" type="text/css" href="\5SeasonsHotel\includes\includeStyle\navigationMenu.css">
+    
+  </head>
+  <body>
+    <?php include('../includes/navigationMenu.php'); ?>
+
+    <section>
+    
+    </section>
+
+    <?php include('../includes/footer.php'); ?>
+  </body>
+</html>
